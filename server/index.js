b49b49d6caa5e7d7753f6fa3c5d6b8b737e2b03b
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-03-14 22:37:18 +0100

    index.hs

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+
+const CONNECTION_URL = ''
+
+mongoose
+  .connect(CONNECTION_URL, { useUnifiedTopology: true, useFindAndModify: true })
+  .then(() => console.log('connected to DB'))
+  .catch(err => console.log(err.message))
+
+const PORT = process.env.PORT || 5000
