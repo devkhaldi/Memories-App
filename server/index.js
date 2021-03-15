45d9cf15a6d52ba13dbad29a6e6f6b4d7211c86d
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-03-15 22:53:46 +0100

    connect to database

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors())
-app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 
-const CONNECTION_URL = ''
+const CONNECTION_URL =
+  'mongodb+srv://admin:Khaldi2000@node-rest-shop.uem3d.mongodb.net/Memories?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
 
 mongoose
   .connect(CONNECTION_URL, { useUnifiedTopology: true, useFindAndModify: true })
-  .then(() => console.log('connected to DB'))
+  .then(() => app.listen(PORT, () => console.log(`Server started at ${PORT}`)))
   .catch(err => console.log(err.message))
 
-const PORT = process.env.PORT || 5000
+// mongoose.use('useFindAndModify', false)
