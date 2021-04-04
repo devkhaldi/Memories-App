3c678dcbee3260ee45e3ce997266d9c170d2f14d
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-04-04 23:35:24 +0100

    memories

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import postRoutes from './routes/posts.js'
 
+const showMessage = () => {
+  
+}
+
 const app = express()
 
 app.use(cors())
 app.use(express.json({ limit: '30mb' }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
+  app.use(express.urlencoded({ limit: '30mb', extended: true }))
 
 const CONNECTION_URL =
   'mongodb+srv://admin:Khaldi2000@node-rest-shop.uem3d.mongodb.net/Memories?retryWrites=true&w=majority'
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
 const postSchema = mongoose.Schema({
-    title : String , 
-    message : String ,
-    creator : String ,
-    tags : [String] , 
-    likeCount : {type: Number , default : 0 } , 
-    selectedFile : String ,
-    createdAt : {type: Date , default : new Date()}
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  likeCount: { type: Number, default: 0 },
+  selectedFile: String,
+  createdAt: { type: Date, default: new Date() },
 })
 
-const Post = mongoose.model('Post', postSchema)
+const Post = mongoose.model("Post", postSchema)
 export default Post
