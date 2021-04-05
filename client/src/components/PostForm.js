030f147d5a2a4ebf8eb6936bd228b9391f5919f5
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-04-05 22:28:02 +0100

    Post form

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -5,17 +5,22 @@ const PostForm = ({ post }) => {
     <div className='modal fade' id='postModal' tabindex='-1'>
       <div className='modal-dialog'>
         <div className='modal-content'>
-          <div className='modal-header'>
-            <h5 className='modal-title'>
-              {post ? `Edit ${post.title}` : "Create new post"}
-            </h5>
-            <button
-              type='button'
-              className='btn-close'
-              data-bs-dismiss='modal'
-              aria-label='Close'></button>
+          <h5 className='modal-title'>
+            {post ? `Edit ${post.title}` : "Create new post"}
+          </h5>
+          <div className='modal-body'>
+            {/* post form */}
+            {/* prettier-ignore */}
+            <form>
+                <input type="text" className='form-control' placeholder='Creator'/>
+                <input type="text" className='form-control' placeholder='Title'/>
+                <textarea cols="30" className='form-control' rows="10" placeholder='Message'></textarea>
+                <input type="text" className='form-control' placeholder='Tags ( Coms separated)'/>
+                <input type="file"/>
+                <input type="submit"/>
+                <input type="clear"/>
+            </form>
           </div>
-          <div className='modal-body'></div>
         </div>
       </div>
     </div>,
