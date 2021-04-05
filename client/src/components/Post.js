0e5708a58090a74d62d2874cd7d579081ae5cf0d
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-04-05 21:08:31 +0100

    Post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import { AiOutlineLike } from "react-icons/ai"
+import { AiOutlineDelete } from "react-icons/ai"
+import { BsThreeDots } from "react-icons/bs"
+import moment from "moment"
 
-const Post = () => {
-    return (
-        <div>
-            
+const Post = ({ post }) => {
+  return (
+    <section className='post'>
+      <div className='img-container'>
+        <div className='img-container-header'>
+          <span className='creator'>{post.creator}</span>
+          <BsThreeDots />
         </div>
-    )
+        <span className='date'>{moment(post.createdAt).fromNow()}</span>
+        <img src={post.img} alt='post image' />
+      </div>
+      <div className='tags'></div>
+      <div className='title'>{post.title}</div>
+      <div className='description'>{post.description}</div>
+      <div className='actions'>
+        <button className='like'>
+          <AiOutlineLike />
+          Like {post.likeCount}
+        </button>
+        <button className='delete'>
+          <AiOutlineDelete />
+          Delete
+        </button>
+      </div>
+    </section>
+  )
 }
 
 export default Post
