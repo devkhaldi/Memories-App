e99d81275bd8d729281c6ef1c07807298c373018
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2021-04-05 22:17:59 +0100

    Post Modal

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react"
 import logo from "../images/logo.png"
+import { IoIosAdd } from "react-icons/io"
 
 const Header = () => {
   return (
     <header>
       <h2>Memories</h2>
       <img src={logo} alt='logo' />
+      <button data-bs-toggle='modal' data-bs-target='#postModal'>
+        <IoIosAdd />
+      </button>
     </header>
   )
 }
--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import ReactDom from "react-dom"
 
-const PostForm = () => {
-    return (
-        <div>
-            
+const PostForm = ({ post }) => {
+  return ReactDom.createPortal(
+    <div className='modal fade' id='postModal' tabindex='-1'>
+      <div className='modal-dialog'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h5 className='modal-title'>
+              {post ? `Edit ${post.title}` : "Create new post"}
+            </h5>
+            <button
+              type='button'
+              className='btn-close'
+              data-bs-dismiss='modal'
+              aria-label='Close'></button>
+          </div>
+          <div className='modal-body'></div>
         </div>
-    )
+      </div>
+    </div>,
+    document.getElementById("createPost")
+  )
 }
 
 export default PostForm
